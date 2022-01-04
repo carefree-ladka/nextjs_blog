7fe79f6b5dc3dd43aa6d758967af4d03c926ad37
Pawan Kumar <30733689+carefree-ladka@users.noreply.github.com>
2022-01-04 16:18:37 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
                     <Image src="/pawan.jpg" height={200} width={200} priority alt="pawan" />
                 </div>
                 <p className='tracking-wide leading-7'>
-                    I'm a Software Engineer who enjoys coding and designing responsive web apps. My favorite technologies right now are: <mark>React.js, Next.js, Typescript, GraphQL, Node.js and MongoDB</mark>.
+                    I&apos;m a Software Engineer who enjoys coding and designing responsive web apps. My favorite technologies right now are: <mark>React.js, Next.js, Typescript, GraphQL, Node.js and MongoDB</mark>.
                 </p>
                <div className=" tracking-wider  inline-block mt-3">
                <a className='underline text-blue-600 hover:text-blue-800 tracking-widest' href='/Resume.pdf' rel="noopener noreferrer" target="_blank">Download my resume</a>
